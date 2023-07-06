daa201331b8db7ba865a3d8a5f254502a529b04a
DavidTMaciel <davidmacieljr122@gmail.com>
2023-07-06 20:56:54 -0300

    Inicialização da aplicação

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+require('dotenv').config();
+require("./config/database").connect();
+const User = require('./model/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+
+const app = express();
+
+app.use(express.json());
+
+
+app.post("/register", (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+        if(!(name && email && password)){
+            res.status(404).send("All input is required");
+        }
+
+        encryptedPassword = bcrypt.hash(password, 10);
+
+        const user = User.create({
+            name,
+            email: email.toLowerCase(), 
+            password: encryptedPassword,
+          });
+        
+        const token = jwt.sign(
+            {user_id: user.id, email},
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "1h",
+            }
+        );
+        user.token = token;
+
+        res.status(200).json(user)
+    }catch(err){
+        
+        console.log(err);
+    }
+});
+
+module.exports = app;
